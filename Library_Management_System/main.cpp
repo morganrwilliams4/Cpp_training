057eb96b669226cbcf5346bc0924a822b6c79fe1
Morgan Rhys Williams <morganrwilliams4@gmail.com>
2025-12-08 13:25:38 +0000

    Implement main functionality for library system

---
+++ Library_Management_System/main.cpp
@@ -0,0 +1,26 @@
+#include "Library.hpp"
+#include <iostream>
+
+using namespace std;
+
+double libraryBudget = 5000;
+
+int main() {
+  cout << "Current library budget is: " << libraryBudget << endl;
+
+  Library myLibrary;
+
+  myLibrary.addBook("The Great Gatsby", 300);
+  myLibrary.addBook("1984", 400);
+  myLibrary.addBook("To Kill a Mockingbird", 350);
+
+  myLibrary.listBooks();
+
+  myLibrary.reserveBook("1984");
+
+  myLibrary.borrowBook("To Kill a Mockingbird");
+
+  int total = Library::getTotalBorrowedBooks();
+  cout << "The total numnber of borrowed books is: " << total << endl; 
+
+}
