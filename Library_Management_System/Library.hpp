6e55aae1b846729dd73afd4abb8872e86dc02cfa
Morgan Rhys Williams <morganrwilliams4@gmail.com>
2025-12-08 13:24:30 +0000

    Add Library class with book management functions

---
+++ Library_Management_System/Library.hpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <string>
+
+using namespace std;
+
+extern double libraryBudget;
+
+class Library {
+private:
+  vector<string> bookList;
+  static int totalBorrowedBooks;
+  
+  mutable int reservedCount;
+  mutable vector<string> reservedBooks;
+
+public:
+  Library();
+
+  void addBook (string title, double bookCost);
+  bool borrowBook(string title);
+  void listBooks();
+  
+  static int getTotalBorrowedBooks();
+
+  void reserveBook(string title) const;
+};
